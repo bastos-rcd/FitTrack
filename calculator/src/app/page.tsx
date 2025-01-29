6e8ae578573d3de61baff94cc174cc33bf73dc2b
Bastien <record.bastien@gmail.com>
2025-01-29 14:08:00 +0100

    feat(home): export data in popup

--- calculator/src/app/page.tsx
+++ calculator/src/app/page.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import useMealStore from "@/store/mealStore";
 import { Food } from "@/models/food";
+import { useState } from "react";
 
 const HomePage = () => {
   const { meal, calculateTotal } = useMealStore();
@@ -10,10 +11,27 @@ const HomePage = () => {
   const { totalCalories, totalProteins, totalCarbs, totalFats } =
     calculateTotal();
 
-  const onExportMacros = () => {};
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="w-full md:w-1/4 h-full flex flex-col items-center">
+      {isOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-2">
+          <div className="w-full md:w-1/4 flex flex-col justify-center items-center p-6 rounded-xl gap-y-2 bg-white">
+            <p className="text-gray-500">Calories : {totalCalories} kcal</p>
+            <p className="text-green-500">Protéines : {totalProteins} g</p>
+            <p className="text-amber-500">Glucides : {totalCarbs} g</p>
+            <p className="text-red-500">Lipides : {totalFats} g</p>
+            <button
+              onClick={() => setIsOpen(false)}
+              className="mt-4 p-2 bg-rose-500 text-white rounded-xl"
+            >
+              FERMER
+            </button>
+          </div>
+        </div>
+      )}
+
       <p className="text-xl font-bold p-2">{totalCalories} kcal</p>
       <div className="w-full flex flex-row justify-between p-2 text-sm">
         <p className="text-green-500 font-bold">P : {totalProteins} g</p>
@@ -22,7 +40,7 @@ const HomePage = () => {
       </div>
       <div className="w-full flex flex-row justify-evenly p-2 text-sm text-white">
         <button
-          onClick={onExportMacros}
+          onClick={() => setIsOpen(true)}
           className="p-1 text-center justify-center items-center rounded-xl bg-violet-300"
         >
           <img src="/export.png" className="w-8" />
