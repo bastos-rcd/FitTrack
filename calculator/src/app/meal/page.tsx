7533f552e03e84a09e078a6912e06ddc05825663
Bastien <record.bastien@gmail.com>
2025-01-29 14:51:14 +0100

    feat(meal): search food

--- calculator/src/app/meal/page.tsx
+++ calculator/src/app/meal/page.tsx
@@ -1,7 +1,47 @@
-export default function Meal() {
+"use client";
+
+import Link from "next/link";
+import useFoodStore from "@/store/foodStore";
+import useMealStore from "@/store/mealStore";
+import { Food } from "@/models/food";
+import { useState } from "react";
+
+const MealPage = () => {
+  const { addFood } = useMealStore();
+  const { food, fetchFood } = useFoodStore();
+
+  const [barcode, setBarCode] = useState<number | "">("");
+
+  const onSearchFood = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await fetchFood(barcode.toString());
+  };
+
   return (
-    <div>
-      <h1>Meal</h1>
+    <div className="w-full md:w-1/4 h-full flex flex-col items-center">
+      <form
+        onSubmit={onSearchFood}
+        className="w-full flex flex-row justify-center items-center p-2 gap-x-2"
+      >
+        <input
+          type="number"
+          placeholder="Code barre"
+          value={barcode}
+          onChange={(e) =>
+            setBarCode(e.target.value ? parseFloat(e.target.value) : "")
+          }
+          className="w-full bg-white text-sm p-2 rounded-xl"
+          required
+        />
+        <button
+          type="submit"
+          className="p-1 text-center justify-center items-center rounded-xl bg-violet-300"
+        >
+          <img src="/search.png" className="w-8" />
+        </button>
+      </form>
     </div>
   );
-}
+};
+
+export default MealPage;
--- calculator/src/store/foodStore.ts
+++ calculator/src/store/foodStore.ts
@@ -2,17 +2,17 @@ import { create } from 'zustand';
 import { Food } from '@/models/food';
 
 interface FoodStore {
-    foods: Food[];
+    food: Food;
     isLoading: boolean;
     error: string | null;
-    fetchFoods: (barcode: string) => Promise<void>;
+    fetchFood: (barcode: string) => Promise<void>;
 }
 
 const useFoodStore = create<FoodStore>((set) => ({
-    foods: [],
+    food: new Food('', '', 0, 0, 0, 0),
     isLoading: false,
     error: null,
-    fetchFoods: async (barcode: string) => {
+    fetchFood: async (barcode: string) => {
         set({ isLoading: true, error: null });
         try {
             const response = await fetch(`https://world.openfoodfacts.org/api/v3/product/${barcode}?product_type=food&cc=fr&lc=fr&fields=product_name%2Cnutriments`);
@@ -21,6 +21,7 @@ const useFoodStore = create<FoodStore>((set) => ({
             if (data.status === 'success') {
                 const product = data.product;
                 const food: Food = new Food(
+                    barcode,
                     product.product_name,
                     product.nutriments['energy-kcal_value'] || 0,
                     product.nutriments['proteins_value'] || 0,
@@ -28,7 +29,7 @@ const useFoodStore = create<FoodStore>((set) => ({
                     product.nutriments['fat_value'] || 0,
                 );
 
-                set({ foods: [food], isLoading: false });
+                set({ food: food, isLoading: false });
             } else {
                 set({ error: 'Product not found or invalid barcode', isLoading: false });
             }
