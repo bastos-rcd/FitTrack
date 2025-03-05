caf145802d0b2aebbb0b0c1de63c7f00749179b5
Bastien Record <record.bastien@gmail.com>
2025-03-05 19:15:43 +0100

    feat(models): add meal class

---
+++ src/models/meal.ts
@@ -0,0 +1,26 @@
+import { Food } from "./food";
+
+export class Meal {
+    private foods: Food[];
+
+    constructor() {
+        this.foods = [];
+    }
+
+    public addFood(_food: Food): void {
+        this.foods.push(_food);
+    }
+
+    public getFoods(): Food[] {
+        return this.foods;
+    }
+
+    public getMacros(): any {
+        return {
+            totalCalories: this.foods.reduce((sum: number, food) => sum + food.getCalories(), 0),
+            totalProteins: this.foods.reduce((sum: number, food) => sum + food.getProteins(), 0),
+            totalCarbs: this.foods.reduce((sum: number, food) => sum + food.getCarbs(), 0),
+            totalFats: this.foods.reduce((sum: number, food) => sum + food.getFats(), 0)
+        }
+    }
+}
