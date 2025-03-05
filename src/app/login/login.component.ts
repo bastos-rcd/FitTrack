319ccf9f41b310d7a48392d6c96137598e4edcdd
Bastien Record <record.bastien@gmail.com>
2025-03-05 17:37:46 +0100

    feat(auth): connexion activate

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  model: any = {};
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  public onSubmit() {
+    this.authService.login(this.model.email, this.model.password).then((user) => {
+      if (user) {
+        this.router.navigate(['/']);
+      } else {
+        alert('Invalid email or password');
+      }
+    });
+  }
 }
