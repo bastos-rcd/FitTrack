513deee1b983f20614b84a788b0047c871b8937a
Bastien Record <record.bastien@gmail.com>
2025-03-06 17:50:09 +0100

    feat(calculator): add food in mealStore & show meal

--- src/app/food/food-get/food-get.component.ts
+++ src/app/food/food-get/food-get.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Meal } from '../../../models/meal';
 import { MealStoreService } from '../../../services/meal-store.service';
+import { FoodService } from '../../../services/food.service';
+import { Food } from '../../../models/food';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-food-get',
@@ -11,13 +14,40 @@ import { MealStoreService } from '../../../services/meal-store.service';
 
 export class FoodGetComponent {
   public meal: Observable<Meal>;
+  public food: Food | undefined;
+  public foods: Food[] = [];
+
   public model: any = {};
 
   constructor(
-    private mealStore: MealStoreService
+    private mealStore: MealStoreService,
+    private foodService: FoodService,
+    private router: Router
   ) {
     this.meal = this.mealStore.meal$;
+    this.foodService.getFoods().then((foods) => {
+      this.foods = foods;
+    });
   }
 
-  public onSubmit() { }
+  public onSubmit() {
+    if (this.model.foodName && this.model.portion) {
+      const tempFood = this.foods.find((f) => f.getName() === this.model.foodName)!;
+      const tempPortion = tempFood.getMacros(this.model.portion);
+      this.food = new Food(
+        tempFood.getName(),
+        tempPortion.calories,
+        tempPortion.proteins,
+        tempPortion.carbs,
+        tempPortion.fats
+      );
+    }
+  }
+
+  public onAddFood() {
+    if (this.food) {
+      this.mealStore.addFood(this.food);
+      this.router.navigate(['/calculator']);
+    }
+  }
 }
